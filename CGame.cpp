c522d3b16cc4305867931cf1a2b67c2901d15eac
Z-dashas <darazvereva838@gmail.com>
2025-12-01 14:22:58 +0500

    Update CGame.cpp
    исправлены ошибки

---
+++ CGame.cpp
@@ -0,0 +1,10 @@
+#include "CGame.h"
+
+#include <iostream>
+#include <cstring>
+#include <conio.h>
+
+// форматная строка для форматирования результата игры
+const char *recordFormatStr = "%-25s %10f %4u %7.2f %s";
+
+SRecord::SRecord(){
