4c1ac551093cdc4ce36d5437f237f18072d83ca7
Z-dashas <darazvereva838@gmail.com>
2025-12-08 13:27:35 +0500

    Update CGame.cpp
    250

--- CGame.cpp
+++ CGame.cpp
@@ -5,6 +5,7 @@
 #include <conio.h>
 
 // форматная строка для форматирования результата игры
-const char *recordFormatStr = "%-25s %10f %4u %7.2f %s";
+const char *recordFormatStr = "%-250s %10f %4u %7.2f %s";
 
 SRecord::SRecord(){
+
